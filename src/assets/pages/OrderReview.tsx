a86dadc95387e007f56fdbbf31c7f2858fca3409
Lucas Grander <lucasgrander@gmail.com>
2024-06-18 01:15:06 -0300

    componente (TimeLine) finalizado

--- src/assets/pages/OrderReview.tsx
+++ src/assets/pages/OrderReview.tsx
@@ -7,6 +7,11 @@ import clocktime_icon from '../../../public/clocktime-circle.png'
 import empty_icon from '../../../public/empty-circle.png'
 
 const OrderReview = () => {
+
+  const handleReturnShoppingCart = () => {
+    alert("Voltar para a tela do carrinho");
+}
+
   return (
     <>
         <div className="container-orderReview">
@@ -15,6 +20,7 @@ const OrderReview = () => {
                 displayIconCart={false}
                 displayCounter={false}
                 displayTitle={false}
+                onClick={handleReturnShoppingCart}
             />
 
             <TimelineOrder
@@ -22,7 +28,20 @@ const OrderReview = () => {
             orderReview={clocktime_icon}
             payment={empty_icon}
             purshaceConfirmation={empty_icon} 
+
+            colorDescOne="#0FA958"
+            opacityDescOne="1"
+            colorDescTwo="#699BF7"
+            opacityDescTwo="1"
+            colorDescThree="black"
+            opacityDescThree="0.4"
+            colorDescFour="black"
+            opacityDescFour="0.4"
+
+            classNameRevisao='clocktime'
             />
+
+            
         </div>    
     </>
   )
