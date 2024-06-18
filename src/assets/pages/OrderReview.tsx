ab9e9994d810935e56b3e27a1bc118e1e3f9f804
Lucas Grander <lucasgrander@gmail.com>
2024-06-18 20:13:11 -0300

    tela de resumo do pedido finalizada

--- src/assets/pages/OrderReview.tsx
+++ src/assets/pages/OrderReview.tsx
@@ -2,14 +2,20 @@ import './OrderReview.css'
 import Header from '../components/Header'
 import TimelineOrder from '../components/TimelineOrder'
 
+import paodequeijo from '../../../public/paodequijo-criativo.jpeg'
 import confirmed_icon from '../../../public/concluded-circle.png'
 import clocktime_icon from '../../../public/clocktime-circle.png'
 import empty_icon from '../../../public/empty-circle.png'
+
 
 const OrderReview = () => {
 
   const handleReturnShoppingCart = () => {
     alert("Voltar para a tela do carrinho");
+}
+
+  const handleMethodPay = () => {
+    alert("Escolher método de pagamento");
 }
 
   return (
@@ -41,7 +47,33 @@ const OrderReview = () => {
             classNameRevisao='clocktime'
             />
 
-            
+            <div className="content-orderReview">
+              <div className="content-title-orderReview">
+                  <span>Revise o seu pedido:</span>
+              </div>
+
+              <div className="box-orders">
+                <div className="order">
+                  <div className="order-img">
+                    <img src={paodequeijo} alt="" />
+                  </div>
+
+                  <div className="order-infos">
+                    <span className='order-nameProduct' >Pão de Queijo</span>
+                    <span className='order-qttProduct' >Unidades: 2</span>
+                  </div>
+                  
+                  <div className="order-price">
+                    <span>R$</span> <span>6,00</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="box-metodPay">
+                <button onClick={handleMethodPay} >Escolher método de pagamento</button>
+              </div>
+            </div>
+
         </div>    
     </>
   )
