7fe75d9d5e670da43ca0d8d00821362fe80cf1aa
JOAO VICTOR SILVA <joao.34907@aluno.pr.senac.br>
2024-06-21 10:36:19 -0300

    implementação método de pagamento

--- src/assets/pages/MetodoDePagamento.tsx
+++ src/assets/pages/MetodoDePagamento.tsx
@@ -9,6 +9,8 @@ const MetodoDePagamento = () => {
             <div className='topo'>
                  <div className='logo'>
                      <img src="/Senac_logo.svg.png"/>
+                         <i className="fa fa-hand-o-left" aria-hidden="true"></i>
+                         </div>
                          <div className='text'>
                              <h6>Nome para retirada no pedido:</h6>
                                 <div className='text_box'>
@@ -16,22 +18,21 @@ const MetodoDePagamento = () => {
                                         <div className='text_E_m_P'>
                                              <h5>Escolha o método de pagamento:</h5>
                                         <div className='pagamento'>
-                                            <h6> PIX</h6>
-                                            <h6> Dinheiro</h6>
-                                            <h6> Cartão de Crédito</h6>
-                                             <h6> Cartão de Débito</h6>
+                                            <h6><input type="radio"/> PIX</h6>
+                                            <h6> <input type="radio"/>Dinheiro</h6>
+                                            <h6> <input type="radio"/>Cartão de Crédito</h6>
+                                             <h6> <input type="radio"/> Cartão de Débito</h6>
                                             <div className='button'>
                                                 <MyButton>
                                                 Código para retirada do pedido
                                                 </MyButton>
+                                                </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
         </>
     )
 }
