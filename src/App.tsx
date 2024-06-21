fe2b9f70cfe3e97fa78007339a8e9004a5a8126a
ZUMBI06 <bastoscamargojhonatas@gmail.com>
2024-06-21 09:39:00 -0300

    criado e finalizado tela do código

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
+import Final from './assets/pages/Final'
 import Main from './assets/pages/Main'
 import OrderReview from './assets/pages/OrderReview'
 
 function App() {
   return (
     <>
-      <Main></Main>
+      {/* <Main></Main> */}
+      <Final></Final>
     </>
   )
 }
