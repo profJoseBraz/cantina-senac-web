653a1c15877ea78f9f8054c02a04764e9b69c139
JOAO VICTOR SILVA <joao.34907@aluno.pr.senac.br>
2024-06-26 11:16:26 -0300

    metodo2606

--- src/assets/pages/MetodoDePagamento.tsx
+++ src/assets/pages/MetodoDePagamento.tsx
@@ -3,6 +3,8 @@ import MyButton from '../components/MyButton'
 import TimelineOrder from '../components/TimelineOrder'
 import './MetodoDePagamento.css'
 import vc from '../votarcardapio.png'
+import visa from '../visa-logo-11.jpg'
+import mester from '../mastercard-novo-logo.jpg'
 import paodequeijo from '../../../public/paodequijo-criativo.jpeg'
 import confirmed_icon from '../../../public/concluded-circle.png'
 import clocktime_icon from '../../../public/clocktime-circle.png'
@@ -39,20 +41,33 @@ const MetodoDePagamento = () => {
                          <div className='text'>
                              <h6>Nome para retirada no pedido:</h6>
                                 <div className='text_box'>
-                                         <input></input>
+                                        <input type='text' />
                                         <div className='text_E_m_P'>
                                              <h5>Escolha o método de pagamento:</h5>
                                         <div className='pagamento'>
-                                            <h6><input type="radio"/> PIX</h6>
-                                            <h6> <input type="radio"/>Dinheiro</h6>
-                                            <h6> <input type="radio"/>Cartão de Crédito</h6>
-                                             <h6> <input type="radio"/> Cartão de Débito</h6>
-                                            <div className='button'>
+                                            
+                                            <h6><input type="radio" id="1" name='tipo-pagamento'/><a>PIX</a></h6>
+                                            <h6> <input type="radio" id="2" name='tipo-pagamento'/><a>Dinheiro</a></h6>
+                                            <div className='payment-method'>
+                                                <h6> <input type="radio" id="3" name='tipo-pagamento'/><a>Cartão de Crédito</a></h6>
+                                                <div className='card'>
+                                                    <img src={mester}></img>
+                                                    <img src={visa}></img>
+                                                </div>
+                                            </div>
+
+                                            <h6> <input type="radio" id="4" name='tipo-pagamento' /><a> Cartão de Débito</a></h6>
+                                            <div className='card'>
+                                                    <img src={mester}></img>
+                                                    <img src={visa}></img>
+                                                </div>
+                                            
+                                        </div>
+                                        <div className='button'>
                                                 <MyButton>
                                                 Código para retirada do pedido
                                                 </MyButton>
                                                 </div>
-                                        </div>
                                     </div>
                                 </div>
                             </div>
