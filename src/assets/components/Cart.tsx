7a53f3497285f1540fe28a18daa99a797ddfb5af
LucasGrander <157768888+LucasGrander@users.noreply.github.com>
2024-06-27 17:29:23 -0300

    adicionando e removendo produtos do carrinho de compras

--- src/assets/components/Cart.tsx
+++ src/assets/components/Cart.tsx
@@ -5,44 +5,33 @@ import triangleBG from '../../../public/triangle-decoration-cart.svg'
 
 interface Props {
   onClickConfirm?: () => void
-  onClickDeleteProdCart?: () => void
+  onClickDeleteProdCart?: (productId: any) => void;
+  onClickIncreaseAmount?: (productId: any) => void;
+  onClickDecreaseAmount?: (productId: any) => void;
+
   style?: CSSProperties
+  productsCart: any
 }
 
-const Cart = ({onClickConfirm, onClickDeleteProdCart, style}: Props) => {
+const Cart = ({onClickConfirm, onClickDeleteProdCart,  onClickDecreaseAmount,  onClickIncreaseAmount, style, productsCart, }: Props) => {
 
-  // interação quantiade dos produtos
-  const [amountProduto, setAmountProduto] = useState(1)
-  const handleIncreaseTotal = () => {
-    setAmountProduto(amountProduto + 1)
-  }
-
-  const handleDecreaseTotal = () => {
-    if(amountProduto <= 1 ){
-      setAmountProduto(amountProduto)
-    }
-
-    else{
-      setAmountProduto(amountProduto - 1)
-    }
-  }
 
   // equações para preços dos produtos
-  const stringProductPriceEach = '2.50'
-  const integerProductPriceEach = parseFloat(stringProductPriceEach)
-  const [subTotalProduto, setSubTotalProduto] = useState(0) 
-  const [totalProduto, setTotalProduto] = useState(0) 
+  // const stringProductPriceEach = '2.50'
+  // const integerProductPriceEach = parseFloat(stringProductPriceEach)
+  // const [subTotalProduto, setSubTotalProduto] = useState(0) 
+  // const [totalProduto, setTotalProduto] = useState(0) 
 
-  const handleSetPriceSubTotalAndTotal = () => {
-    setSubTotalProduto(amountProduto * integerProductPriceEach)
-    setTotalProduto(subTotalProduto)
-  }
+  // const handleSetPriceSubTotalAndTotal = () => {
+  //   setSubTotalProduto({produc} * integerProductPriceEach)
+  //   setTotalProduto(subTotalProduto)
+  // }
 
-  useEffect(() => {
-    handleSetPriceSubTotalAndTotal()
-  }, [amountProduto, subTotalProduto])
+  // useEffect(() => {
+  //   handleSetPriceSubTotalAndTotal()
+  // }, [cartAmountProduto, subTotalProduto])
+
   
-
   return (
     <>
         <aside style={style} className="container-cart">
@@ -52,44 +41,47 @@ const Cart = ({onClickConfirm, onClickDeleteProdCart, style}: Props) => {
 
             <div className="content-cart">
 
-              <div className="box-produto-cart">
-              <i onClick={onClickDeleteProdCart} className="fa fa-trash" aria-hidden="true"></i>
-                <div className="img-counter-cart">
-                  <img src={paodequeijo} alt="" />
+              {productsCart.map((product: any) => (
+                <div key={product.id_produto} className="box-produto-cart">
+                  <i onClick={() => onClickDeleteProdCart && onClickDeleteProdCart(product.id_produto)} className="fa fa-trash" aria-hidden="true"></i>
+                  <div className="img-counter-cart">
+                    <img src={paodequeijo} alt="" />
 
-                  <div className="counter-cart">
+                    <div className="counter-cart">
 
-                    <div onClick={handleDecreaseTotal} className="minus">
-                      <span>-</span>
+                      <div onClick={onClickDecreaseAmount} className="minus">
+                        <span>-</span>
+                      </div>
+
+                      <div className="numberQtt">
+                        <span>{product.quantidade}</span>
+                      </div>
+
+                      <div onClick={onClickIncreaseAmount} className="sum">
+                        <span>+</span>
+                      </div>
+
                     </div>
-
-                    <div className="numberQtt">
-                      <span>{amountProduto}</span>
-                    </div>
-
-                    <div onClick={handleIncreaseTotal} className="sum">
-                      <span>+</span>
-                    </div>
-
                   </div>
-                </div>
 
-                <div className="infos-produto-cart">
-                    <span className='name-produto'>Pão de queijo</span>
-                    <span className='preco-string'>Valor: <span className='preco-produto'>R$ {stringProductPriceEach} / un</span></span>
+                  <div className="infos-produto-cart">
+                    <span className='name-produto'>{product.nome}</span>
+                    <span className='preco-string'>Valor: <span className='preco-produto'>R$ {product.valor} / un</span></span>
 
                     <div className="subtotal-produto">
-                      <span className='subtotal-string'>Subtotal: <span className='subtotal-preco'>R$ {subTotalProduto}</span></span>  
+                      <span className='subtotal-string'>Subtotal: <span className='subtotal-preco'>R$ 00</span></span>  
                     </div>             
                   </div>
                   
               </div>
+              ))}
+
             </div>
 
             <button onClick={onClickConfirm} >Confirmar Pedido <i className="fa fa-arrow-right" aria-hidden="true"></i></button>
 
             <div className="total-cart">
-              <span className='total-string'>Total: <span className='total-preco'>R$ {totalProduto}</span></span>
+              <span className='total-string'>Total: <span className='total-preco'>R$ 00</span></span>
             </div>
 
             <img className='triangle-one' src={triangleBG} alt="" />
--- src/assets/pages/Main.tsx
+++ src/assets/pages/Main.tsx
@@ -10,53 +10,26 @@ import axios from "axios";
 function Main() {
 
 // --- puxar todos os produtos disponíveis no dia ao entrar na página ---
-const handleGetAllProducts = async () => {
-    const res = await axios.get("https://cantina-senac-api-prod.up.railway.app/production")
-    setProductsOnShop(res.data)
-}
+    const handleGetAllProducts = async () => {
+        const res = await axios.get("https://cantina-senac-api-prod.up.railway.app/production")
+        setProductsOnShop(res.data)
+    }
     useEffect(() => {
         handleGetAllProducts()
       }, [])
 
 
-// --- ação ao clickar no carrinho de compras ---
-    const handleCart = () => {
-        if(cartOpen){
-            setCartOpen(false)
+// --- funções abaixo para aplicar filtros de categoria nos produtos ---
+    const [categoryId, setCategoryId] = useState(0)
+    const handleGetProdutsByCategory = async () => {
+        if(categoryId != 0){
+            const res = await axios.get(`https://cantina-senac-api-prod.up.railway.app/production/products?categoryId=${categoryId}`)
+            setProductsOnShop(res.data)
         }
         else{
-            setCartOpen(true)
+            handleGetAllProducts()
         }
     }
-
-
-// --- botão dentro carrinho de compras --> Ir para a tela de Revisão do Pedido ---
-    const handleConfirmOrder = () => {
-        alert("Confirmar pedido");
-    }
-
-
-// --- remover e adicionar produto do carrinho de compras ---
-    const handleDeleteProduct = () => {
-        alert("Remover produto");
-    }
-
-    const handleAddProduct = () => {
-        alert("ok");
-    };
-
-
-// --- funções abaixo para aplicar filtros de categoria nos produtos ---
-const [categoryId, setCategoryId] = useState(0)
-const handleGetProdutsByCategory = async () => {
-    if(categoryId != 0){
-        const res = await axios.get(`https://cantina-senac-api-prod.up.railway.app/production/products?categoryId=${categoryId}`)
-        setProductsOnShop(res.data)
-    }
-    else{
-        handleGetAllProducts()
-    }
-}
 
     const handleOnClickTodos = () => {
         handleGetAllProducts()
@@ -85,12 +58,64 @@ const handleGetProdutsByCategory = async () => {
 // -----------------------------------------------
 
 
+// --- ação ao clickar no carrinho de compras ---
+    const handleCart = () => {
+        if(cartOpen){
+            setCartOpen(false)
+        }
+        else{
+            setCartOpen(true)
+        }
+    }
+
+
+// --- botão dentro carrinho de compras --> Ir para a tela de Revisão do Pedido ---
+    const handleConfirmOrder = () => {
+        alert("Confirmar pedido");
+    }
+
+
+// --- remover e adicionar produto do carrinho de compras ---
+    const handleAddProduct = (product: any) => {
+        setProductsOnCart([...productsOnCart, product])
+        console.log(productsOnCart)
+    }
+
+    const handleDeleteProduct = (productId: any) => {
+        setProductsOnCart(productsOnCart.filter(product => product.id_produto != productId));
+      }
+
+// interação quantiade dos produtos
+    const [cartAmountProduto, setCartAmountProduto] = useState(1)
+
+    const handleIncreaseTotal = (productId: any) => {
+        setProductsOnCart(productsOnCart.map(product =>
+        product.id_produto === productId ? { ...product, quantidade: product.quantidade + 1 } : product
+      ))
+    }
+
+    const handleDecreaseTotal = (productId: any) => {
+        if(cartAmountProduto <= 1 ){
+            setProductsOnCart(productsOnCart.map(product =>
+            product.id_produto === productId ? { ...product, quantidade: product.quantidade} : product
+            ))
+        }
+
+        else{
+            setProductsOnCart(productsOnCart.map(product =>
+            product.id_produto === productId ? { ...product, quantidade: product.quantidade - 1 } : product
+            ))
+        }
+    }
+
 // --- inicializando um array para armazenar os produtos requisitados ---
     const [productsOnShop, setProductsOnShop] = useState<Products[]>([])
 
 // --- estado do carrinho (aberto = true // fechado = false) ---
-     const [cartOpen, setCartOpen] = useState(false)
+    const [cartOpen, setCartOpen] = useState(false)
 
+// --- inicializando um array para armazenar os produtos do carrinho ---
+    const [productsOnCart, setProductsOnCart] = useState<Products[]>([])
 
     return (
         <>
@@ -119,7 +144,7 @@ const handleGetProdutsByCategory = async () => {
                     {productsOnShop.map((product) => (
                         <Product
                         key={product.id_produto}
-                        onClick={handleAddProduct}
+                        onClick={() => handleAddProduct(product)}
                         restrictType={product.restricoes}
                         img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtdBLFQueGRO0CLe8MCGAuuWY4FEa9o6NKUQ&s"
                         name={product.nome}
@@ -134,6 +159,10 @@ const handleGetProdutsByCategory = async () => {
                 style={{width: cartOpen ?  "40vh" : "0", transition: ".4s"}}
                 onClickConfirm={handleConfirmOrder}
                 onClickDeleteProdCart={handleDeleteProduct}
+                onClickIncreaseAmount={handleIncreaseTotal}
+                onClickDecreaseAmount={handleDecreaseTotal}
+
+                productsCart={productsOnCart}
                 /> 
                 
             </div>
